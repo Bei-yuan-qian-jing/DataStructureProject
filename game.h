e2b2f868001670bc3de5900c650a90c7b20b23a3
byqj <1286491245@qq.com>
2020-05-24 09:13:47 +0800

    change the ui slightly zzy

---
+++ game.h
@@ -0,0 +1,26 @@
+#ifndef GAME_H
+#define GAME_H
+#include <QObject>
+#include<iostream>
+const int maxn = 100;
+
+class Game : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Game(QObject *parent = nullptr);
+    int grass_num;
+    int cow_num;
+    int sheep_num;
+    int tiger_num;
+    int board[maxn][maxn];
+    void init(int, int, int, int);
+    void nexterm();
+    void show();
+
+
+signals:
+
+};
+
+#endif // GAME_H
